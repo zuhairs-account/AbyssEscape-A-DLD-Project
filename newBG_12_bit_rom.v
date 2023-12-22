8e9233b4491f40f144cdb41440c895209d5c821a
zuhair <122153573+syedzuhairabbasrizvi@users.noreply.github.com>
2023-12-22 19:14:49 +0500

    Add files via upload

---
+++ newBG_12_bit_rom.v
@@ -0,0 +1,22 @@
+module newBG_rom
+	(
+		input wire clk,
+		input wire [6:0] row,
+		input wire [6:0] col,
+		output reg [11:0] color_data
+	);
+
+	(* rom_style = "block" *)
+
+	//signal declaration
+	reg [6:0] row_reg;
+	reg [6:0] col_reg;
+
+	always @(posedge clk)
+		begin
+		row_reg <= row;
+		col_reg <= col;
+		end
+
+	always @*
+	case ({row_reg, col_reg})
